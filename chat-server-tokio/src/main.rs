bdd1e30d41dd89fef31ff920aaac9c520456157a
Yuvraj Chaturvedi <96003202+Yuvraj004@users.noreply.github.com>
2023-08-28 22:20:44 +0530

    initialization

--- chat-server-tokio/src/main.rs
+++ chat-server-tokio/src/main.rs
@@ -1,3 +1,4 @@
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
 //by deafult rust does not know how to execute the async func so tokio 
 //provides that executor
@@ -6,5 +7,11 @@ async fn main() {
     println!("Hello, world!");
     let listener = TcpListener::bind("localhost:8080").await.unwrap();//returns a future therfore we use await
 
-    let (socket,_addr) = listener.accept().await.unwrap();
+    let (mut _sockett,_addr) = listener.accept().await.unwrap();
+    //to read and put the data which is read from stream
+    let mut buffer = [0u8,255];
+
+    let bytes_read = _sockett.read(&mut buffer).await.unwrap();
+
+    _sockett.write_all(&buffer[..bytes_read]).await.unwrap();//passing the buffer to be written untill the no of bytes read
 }
