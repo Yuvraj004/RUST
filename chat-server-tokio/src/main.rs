5070a18e564c54432987475ccf6c030d560c9e60
Yuvraj Chaturvedi <96003202+Yuvraj004@users.noreply.github.com>
2023-08-29 00:33:32 +0530

    chat server 2.0

--- chat-server-tokio/src/main.rs
+++ chat-server-tokio/src/main.rs
@@ -1,29 +1,21 @@
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpListener;
 use tokio::sync::broadcast;
-//by deafult rust does not know how to execute the async func so tokio
-//provides that executor
-#[tokio::main] //executor macro
+#[tokio::main] 
 async fn main() {
     println!("Hello, world!");
-    let listener = TcpListener::bind("localhost:8080").await.unwrap(); //returns a future therfore we use await
-
+    let listener = TcpListener::bind("localhost:8080").await.unwrap(); 
     let (tx, _rx) = broadcast::channel(10);
 
     loop {
         let (mut _sockett, _addr) = listener.accept().await.unwrap();
-
-        //removing errors by cloning tx
         let tx = tx.clone();
         let mut rx = tx.subscribe();
-        tokio::spawn(async move{
+        tokio::spawn(async move {
             let (reader, mut writer) = _sockett.split();
-        //to read and put the data which is read from stream
             let mut reader = BufReader::new(reader);
-
             let mut line = String::new();
             loop {
-                //problem: same msg is also repeating in same client
                 tokio::select! {
                     result = reader.read_line(&mut line) =>{
                         if result.unwrap() == 0 {
@@ -35,14 +27,12 @@ async fn main() {
                     result = rx.recv()=>{
                         let (msg,other_addr) = result.unwrap();
                         if _addr!=other_addr {
-                            writer.write_all(msg.as_bytes()).await.unwrap(); //passing the buffer to be written untill the no of bytes read
+                            writer.write_all(msg.as_bytes()).await.unwrap(); 
                         }
-                        
                     }
                 }
             }
         });
-        
     }
 }
 //telnet localhost 8080
