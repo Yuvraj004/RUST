98d9abdf3b275a332467beaca9d00122a6e71a07
Yuvraj Chaturvedi <96003202+Yuvraj004@users.noreply.github.com>
2023-08-28 23:52:06 +0530

    echo server

--- chat-server-tokio/src/main.rs
+++ chat-server-tokio/src/main.rs
@@ -1,17 +1,48 @@
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpListener;
-//by deafult rust does not know how to execute the async func so tokio 
+use tokio::sync::broadcast;
+//by deafult rust does not know how to execute the async func so tokio
 //provides that executor
 #[tokio::main] //executor macro
 async fn main() {
     println!("Hello, world!");
-    let listener = TcpListener::bind("localhost:8080").await.unwrap();//returns a future therfore we use await
+    let listener = TcpListener::bind("localhost:8080").await.unwrap(); //returns a future therfore we use await
 
-    let (mut _sockett,_addr) = listener.accept().await.unwrap();
-    //to read and put the data which is read from stream
-    let mut buffer = [0u8,255];
+    let (tx,mut rx) = broadcast::channel::<String>(10);
 
-    let bytes_read = _sockett.read(&mut buffer).await.unwrap();
+    loop {
+        let (mut _sockett, _addr) = listener.accept().await.unwrap();
 
-    _sockett.write_all(&buffer[..bytes_read]).await.unwrap();//passing the buffer to be written untill the no of bytes read
+        //separation of read and write part required becoz Buffreader is taking owneship of the socket earlier
+
+        //removing errors by clonign tx
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
+        tokio::spawn(async move{
+            let (reader, mut writer) = _sockett.split();
+        //to read and put the data which is read from stream
+            let mut reader = BufReader::new(reader);
+
+            let mut line = String::new();
+            loop {
+                
+
+                //to store each line
+                //read_line fucntion adds to the prevous line doesnot generate new one
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+                if bytes_read == 0 {
+                    break;
+                }
+
+                tx.send(line.clone()).unwrap();
+
+                let msg = rx.recv().await.unwrap();
+
+                writer.write_all(msg.as_bytes()).await.unwrap(); //passing the buffer to be written untill the no of bytes read
+                line.clear()
+            }
+        });
+        
+    }
 }
+//telnet localhost 8080
